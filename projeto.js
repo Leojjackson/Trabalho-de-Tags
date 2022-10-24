37578483889611ff4ca28022e25cb9e33dd63cfa
Leojjackson <leojjackson@outlook.com>
2022-10-24 18:40:45 -0300

    Update

---
+++ projeto.js
@@ -0,0 +1,10 @@
+let inicio = document.querySelector('#inicio');
+let sobre = document.querySelector('#sobre');
+let contato = document.querySelector('#contato');
+ let professor = document.querySelector('#professor');
+function clicar()
+{
+    
+    
+    
+}
