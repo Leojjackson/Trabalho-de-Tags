e57eba3a88dd3feae7e0045c5af1b4522b0f7c9d
Leojjackson <leojjackson@outlook.com>
2022-10-24 19:05:53 -0300

    Js adicionado

--- projeto.js
+++ projeto.js
@@ -1,10 +1,31 @@
-let inicio = document.querySelector('#inicio');
-let sobre = document.querySelector('#sobre');
-let contato = document.querySelector('#contato');
- let professor = document.querySelector('#professor');
-function clicar()
-{
-    
-    
-    
+
+function passou(){
+    let sobre = document.querySelector('#sobre');
+    sobre.innerHTML = 'Trabalho do professor Hebert';
+    sobre.style.fontSize ='13pt'
+}
+function sair(){
+    let sobre = document.querySelector('#sobre');
+    sobre.innerHTML = 'Sobre';
+    sobre.style.fontSize ='17pt'
+}
+function clicou(){
+    let contato = document.querySelector('#contato');
+    contato.innerHTML = '(11)91700-5684';
+}
+function saiu(){
+    let contato = document.querySelector('#contato');
+    contato.innerHTML = 'Contato';
+    contato.style.fontSize ='17pt'
+}
+
+function clicarr(){
+    let professor = document.querySelector('#professor');
+    professor.innerHTML = 'Professor Hebert';
+    professor.style.fontSize ='13pt'
+}
+function saiuu(){
+    let professor = document.querySelector('#professor');
+    professor.innerHTML = 'Professor';
+    professor.style.fontSize ='17pt'
 }
